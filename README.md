235f73ccb3960a01bb0f418ff10f7a0a01ef8315
Russell <russell.tarpley@gmail.com>
2017-01-03 19:29:31 -0600

    chore(setup)
    Cloned project setup from ssn-textfield and renamed major variables.
    Building successfully

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
 # mdl-phone-textfield
-A Material Design Lite implementation of a US phone textfield
+An Material Design Lite phone textfield implementation for capturing user entered phones (https://github.com/google/material-design-lite)
+
+[![Bower Version](https://img.shields.io/bower/v/mdl-phone-textfield.svg)](https://github.com/rathxxx/mdl-phone-textfield)
+[![NPM Version](https://img.shields.io/npm/v/mdl-phone-textfield.svg)](https://www.npmjs.com/package/mdl-phone-textfield)
+[![license](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat)](https://github.com/rathxxx/mdl-phone-textfield/blob/master/LICENSE)
+
+[![bitHound Overall Score](https://www.bithound.io/github/rathxxx/mdl-phone-textfield/badges/score.svg)](https://www.bithound.io/github/rathxxx/mdl-phone-textfield)
+[![bitHound Dependencies](https://www.bithound.io/github/rathxxx/mdl-phone-textfield/badges/dependencies.svg)](https://www.bithound.io/github/rathxxx/mdl-phone-textfield/master/dependencies/npm)
+[![bitHound Dev Dependencies](https://www.bithound.io/github/rathxxx/mdl-phone-textfield/badges/devDependencies.svg)](https://www.bithound.io/github/rathxxx/mdl-phone-textfield/master/dependencies/npm)
+[![bitHound Code](https://www.bithound.io/github/rathxxx/mdl-phone-textfield/badges/code.svg)](https://www.bithound.io/github/rathxxx/mdl-phone-textfield)
+
+> A custom textfield implementation of a social security number component for [Material Design Lite](https://github.com/google/material-design-lite)
+
+## Install
+
+Via npm:
+
+````
+npm install mdl-phone-textfield
+````
+
+Then include in your html:
+
+````
+<link rel="stylesheet" href="./bower_components/mdl-phone-textfield/dist/mdl-phone-textfield.min.css">
+...
+<script src="./bower_components/mdl-phone-textfield/dist/mdl-phone-textfield.min.js"></script>
+````
+
+## Basic use
+To use any MDL component, you must include the minified CSS and JavaScript files using standard relative-path references in the `<head>` section of the page, as described in the MDL Introduction.
+
+### To include a MDL **phone textfield** component:
+
+&nbsp;1. Code a `<div>` element to hold the phone text field.
+```html
+<div>
+...
+</div>
+```
+&nbsp;2. Inside the div, code an `<input>` element add an `id` attribute of your choice.
+```html
+<div>
+  <input type="text" id="sample3">
+</div>
+```
+&nbsp;3. Also inside the div, after the `<input>` field, code a `<label>` element with a `for` attribute whose value matches the `input` element's `id` value, and a short string to be used as the field's placeholder text.
+```html
+<div>
+		<input type="text" id="sample3">
+        <label for="sample3">phone Example...</label>
+</div>
+```
+&nbsp;4. Add one or more MDL classes, separated by spaces, to the div container, input field, input label, and error message using the `class` attribute.
+```html
+<div class="mdl-phone-textfield mdl-js-phone-textfield mdl-phone-textfield--floating-label">
+        <input class="mdl-phone-textfield__input" type="text" id="sample3">
+        <label class="mdl-phone-textfield__label" for="sample3">phone Example...</label>
+</div>
+```
+The phone textfield component is ready for use.
+
+#### Examples
+
+phone field with a standard label.
+```html
+<div class="mdl-phone-textfield mdl-js-phone-textfield">
+        <input class="mdl-phone-textfield__input" type="text" id="sample1">
+        <label class="mdl-phone-textfield__label" for="sample1">phone Example...</label>
+</div>
+```
+
+phone field with a floating label.
+```html
+<div class="mdl-phone-textfield mdl-js-phone-textfield mdl-phone-textfield--floating-label">
+        <input class="mdl-phone-textfield__input" type="text" id="sample4">
+        <label class="mdl-phone-textfield__label" for="sample4">phone Example...</label>
+</div>
+```
+
+phone field with a standard label, and error message.
+```html
+<div class="mdl-phone-textfield mdl-js-phone-textfield">
+        <input class="mdl-phone-textfield__input" type="text" id="sample4">
+        <label class="mdl-phone-textfield__label" for="sample4">phone Example...</label>
+		<span class="mdl-phone-textfield__error">(999)999-9999</span>
+</div>
+```
---
+++ src/phone-textfield/phone-textfield.js
@@ -0,0 +1,262 @@
+(function() {
+    'use strict';
+
+    /**
+     * Class constructor for SSN Textfield MDL component.
+     * Implements MDL component design pattern defined at:
+     * https://github.com/jasonmayes/mdl-component-design-pattern
+     *
+     * @constructor
+     * @param {HTMLElement} element The element that will be upgraded.
+     */
+    var MaterialPhoneTextfield = function MaterialPhoneTextfield(element) {
+        this.element_ = element;
+        this.rawValue = "";
+        // Initialize instance.
+        this.init();
+    };
+    window['MaterialPhoneTextfield'] = MaterialPhoneTextfield;
+
+    /**
+     * Store constants in one place so they can be updated easily.
+     *
+     * @enum {string | number}
+     * @private
+     */
+    MaterialPhoneTextfield.prototype.Constant_ = {};
+
+    /**
+     * Store strings for class names defined by this component that are used in
+     * JavaScript. This allows us to simply change it in one place should we
+     * decide to modify at a later date.
+     *
+     * @enum {string}
+     * @private
+     */
+    MaterialPhoneTextfield.prototype.CssClasses_ = {
+        LABEL: 'mdl-phone-textfield__label',
+        INPUT: 'mdl-phone-textfield__input',
+        IS_DIRTY: 'is-dirty',
+        IS_FOCUSED: 'is-focused',
+        IS_DISABLED: 'is-disabled',
+        IS_INVALID: 'is-invalid',
+        IS_UPGRADED: 'is-upgraded',
+        MASKED: 'mdl-phone-textfield--masked'
+    };
+
+    /**
+     * Handle focus.
+     *
+     * @param {Event} event The event that fired.
+     * @private
+     */
+    MaterialPhoneTextfield.prototype.onFocus_ = function (event) {
+        this.element_.classList.add(this.CssClasses_.IS_FOCUSED);
+    };
+
+    /**
+     * Handle lost focus.
+     *
+     * @param {Event} event The event that fired.
+     * @private
+     */
+    MaterialPhoneTextfield.prototype.onBlur_ = function (event) {
+        this.element_.classList.remove(this.CssClasses_.IS_FOCUSED);
+    };
+
+    MaterialPhoneTextfield.prototype.onChange_ = function (event) {
+        var pattern = /^(\d{3})-?(\d{2})-?(\d{4})$/;
+        //Test the input string for basic format (optional '-')
+        if (!pattern.test(this.input_.value)) {
+            this.element_.classList.add(this.CssClasses_.IS_INVALID);
+            return false;
+        } else {
+            var matches = pattern.exec(this.input_.value);
+            this.rawValue = matches[1] + matches[2] + matches[3];
+            //Determine which visual format to use
+            if (this.element_.className.indexOf(this.CssClasses_.MASKED) !== -1) {
+                this.input_.value = "***-" + "**-" + matches[3];
+            } else {
+                this.input_.value = matches[1] + "-" + matches[2] + "-" + matches[3];
+            }
+        }
+    };
+
+    /**
+     * Handle reset event from out side.
+     *
+     * @param {Event} event The event that fired.
+     * @private
+     */
+    MaterialPhoneTextfield.prototype.onReset_ = function (event) {
+        this.updateClasses_();
+    };
+
+    /**
+     * Handle class updates.
+     *
+     * @private
+     */
+    MaterialPhoneTextfield.prototype.updateClasses_ = function () {
+        this.checkDisabled();
+        this.checkValidity();
+        this.checkDirty();
+        this.checkFocus();
+    };
+
+    // Public methods.
+
+    /**
+     * Check the disabled state and update field accordingly.
+     *
+     * @public
+     */
+    MaterialPhoneTextfield.prototype.checkDisabled = function () {
+        if (this.input_.disabled) {
+            this.element_.classList.add(this.CssClasses_.IS_DISABLED);
+        } else {
+            this.element_.classList.remove(this.CssClasses_.IS_DISABLED);
+        }
+    };
+    MaterialPhoneTextfield.prototype['checkDisabled'] =
+        MaterialPhoneTextfield.prototype.checkDisabled;
+
+    /**
+     * Retrieve the raw value of the field
+     * @returns {string} unmasked value
+     */
+    MaterialPhoneTextfield.prototype.getValue = function () {
+        return this.rawValue;
+    };
+    MaterialPhoneTextfield.prototype['getValue'] =
+        MaterialPhoneTextfield.prototype.getValue;
+
+    /**
+    * Check the focus state and update field accordingly.
+    *
+    * @public
+    */
+    MaterialPhoneTextfield.prototype.checkFocus = function () {
+        if (Boolean(this.element_.querySelector(':focus'))) {
+            this.element_.classList.add(this.CssClasses_.IS_FOCUSED);
+        } else {
+            this.element_.classList.remove(this.CssClasses_.IS_FOCUSED);
+        }
+    };
+    MaterialPhoneTextfield.prototype['checkFocus'] =
+      MaterialPhoneTextfield.prototype.checkFocus;
+
+    /**
+     * Check the validity state and update field accordingly.
+     *
+     * @public
+     */
+    MaterialPhoneTextfield.prototype.checkValidity = function () {
+        if (this.input_.validity) {
+            if (this.input_.validity.valid) {
+                this.element_.classList.remove(this.CssClasses_.IS_INVALID);
+            } else {
+                this.element_.classList.add(this.CssClasses_.IS_INVALID);
+            }
+        }
+    };
+    MaterialPhoneTextfield.prototype['checkValidity'] =
+        MaterialPhoneTextfield.prototype.checkValidity;
+
+    /**
+     * Check the dirty state and update field accordingly.
+     *
+     * @public
+     */
+    MaterialPhoneTextfield.prototype.checkDirty = function () {
+        if (this.input_.value && this.input_.value.length > 0) {
+            this.element_.classList.add(this.CssClasses_.IS_DIRTY);
+        } else {
+            this.element_.classList.remove(this.CssClasses_.IS_DIRTY);
+        }
+    };
+    MaterialPhoneTextfield.prototype['checkDirty'] =
+        MaterialPhoneTextfield.prototype.checkDirty;
+
+    /**
+     * Disable text field.
+     *
+     * @public
+     */
+    MaterialPhoneTextfield.prototype.disable = function () {
+        this.input_.disabled = true;
+        this.updateClasses_();
+    };
+    MaterialPhoneTextfield.prototype['disable'] = MaterialPhoneTextfield.prototype.disable;
+
+    /**
+     * Enable text field.
+     *
+     * @public
+     */
+    MaterialPhoneTextfield.prototype.enable = function () {
+        this.input_.disabled = false;
+        this.updateClasses_();
+    };
+    MaterialPhoneTextfield.prototype['enable'] = MaterialPhoneTextfield.prototype.enable;
+
+    /**
+     * Update text field value.
+     *
+     * @param {string} value The value to which to set the control (optional).
+     * @public
+     */
+    MaterialPhoneTextfield.prototype.change = function (value) {
+
+        this.input_.value = value || '';
+        this.updateClasses_();
+    };
+    MaterialPhoneTextfield.prototype['change'] = MaterialPhoneTextfield.prototype.change;
+
+    /**
+   * Initialize element.
+   */
+    MaterialPhoneTextfield.prototype.init = function () {
+
+        if (this.element_) {
+            this.label_ = this.element_.querySelector('.' + this.CssClasses_.LABEL);
+            this.input_ = this.element_.querySelector('.' + this.CssClasses_.INPUT);
+
+            if (this.input_) {
+                this.boundUpdateClassesHandler = this.updateClasses_.bind(this);
+                this.boundFocusHandler = this.onFocus_.bind(this);
+                this.boundBlurHandler = this.onBlur_.bind(this);
+                this.boundResetHandler = this.onReset_.bind(this);
+                this.boundChangeHandler = this.onChange_.bind(this);
+                this.input_.addEventListener('input', this.boundUpdateClassesHandler);
+                this.input_.addEventListener('focus', this.boundFocusHandler);
+                this.input_.addEventListener('blur', this.boundBlurHandler);
+                this.input_.addEventListener('reset', this.boundResetHandler);
+                this.input_.addEventListener('change', this.boundChangeHandler);
+
+                var invalid = this.element_.classList.contains(this.CssClasses_.IS_INVALID);
+                this.updateClasses_();
+                this.element_.classList.add(this.CssClasses_.IS_UPGRADED);
+                if (invalid) {
+                    this.element_.classList.add(this.CssClasses_.IS_INVALID);
+                }
+                if (this.input_.hasAttribute('autofocus')) {
+                    this.element_.focus();
+                    this.checkFocus();
+                }
+                if (this.input_.value.length > 0) {
+                    this.onChange_();
+                }
+            }
+        }
+    };
+
+    // The component registers itself. It can assume componentHandler is available
+    // in the global scope.
+    componentHandler.register({
+        constructor: MaterialPhoneTextfield,
+        classAsString: 'MaterialPhoneTextfield',
+        cssClass: 'mdl-js-phone-textfield',
+        widget: true
+    });
+})();
